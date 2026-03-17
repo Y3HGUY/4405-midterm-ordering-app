f5bd0decec6918e995f90f2753068b33f82559c7
tye yeh <tjyn7m@umsystem.edu>
2026-03-17 17:28:21 -0500

    Initial Commit

---
+++ midterm_ordering_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  midterm_ordering_app
+//
+//  Created by Tye Yeh on 3/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ midterm_ordering_app/midterm_ordering_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  midterm_ordering_appApp.swift
+//  midterm_ordering_app
+//
+//  Created by Tye Yeh on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct midterm_ordering_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
